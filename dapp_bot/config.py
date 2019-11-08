69429ceaba3a8a73732c10b2f425f5c34d796b3c
main_pc <chistiytoillett@gmail.com>
2019-11-09 02:36:53 +0300

    return config.py

---
+++ dapp_bot/config.py
@@ -0,0 +1,20 @@
+import os
+import configparser
+
+def get_root_path(project_dir='dapp_bot'):
+    current_dir = __file__
+    while True:
+        head_dir, tail_dir = os.path.split(current_dir)
+        if head_dir and tail_dir != project_dir:
+            current_dir = head_dir
+        else:
+            return current_dir
+
+config = configparser.ConfigParser()
+config_path = os.path.join(get_root_path(), 'config.ini')
+config.read(config_path)
+
+db_params = dict(config['db'])
+
+token = config['bot']['token']
+
